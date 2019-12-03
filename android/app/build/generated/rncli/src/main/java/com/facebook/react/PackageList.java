d5d1dff2b18325fdf4dd39aa0a0562ea3d9c083f
Felipe Florentino <felipe.florentino01@gmail.com>
2019-12-03 08:11:48 -0300

    Correção Dependências

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.res.Resources;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainPackageConfig;
 import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -30,25 +29,13 @@ import com.beefe.picker.PickerViewPackage;
 public class PackageList {
   private Application application;
   private ReactNativeHost reactNativeHost;
-  private MainPackageConfig mConfig;
-
   public PackageList(ReactNativeHost reactNativeHost) {
-    this(reactNativeHost, null);
+    this.reactNativeHost = reactNativeHost;
   }
 
   public PackageList(Application application) {
-    this(application, null);
-  }
-
-  public PackageList(ReactNativeHost reactNativeHost, MainPackageConfig config) {
-    this.reactNativeHost = reactNativeHost;
-    mConfig = config;
-  }
-
-  public PackageList(Application application, MainPackageConfig config) {
     this.reactNativeHost = null;
     this.application = application;
-    mConfig = config;
   }
 
   private ReactNativeHost getReactNativeHost() {
@@ -70,7 +57,7 @@ public class PackageList {
 
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
-      new MainReactPackage(mConfig),
+      new MainReactPackage(),
       new AsyncStoragePackage(),
       new RNCameraPackage(),
       new RNFSPackage(),
